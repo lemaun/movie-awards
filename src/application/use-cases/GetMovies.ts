d5f2470650ca930a49f265f7d6c4bfe1a7c8c24f
lemaun <leandro@datacampo.com.br>
2026-01-18 14:20:15 -0300

    feat(usecase): implement movies listing and producer intervals

---
+++ src/application/use-cases/GetMovies.ts
@@ -0,0 +1,22 @@
+import { IMovieRepository } from '../../domain/repositories/IMovieRepository.js';
+import { Movie } from '../../domain/entities/Movie.js';
+
+export class GetMovies {
+  constructor(private movieRepository: IMovieRepository) { }
+
+  getAll(): Movie[] {
+    return this.movieRepository.findAll();
+  }
+
+  getByYear(year: number): Movie[] {
+    return this.movieRepository.findByYear(year);
+  }
+
+  getWinners(): Movie[] {
+    return this.movieRepository.findWinners();
+  }
+
+  getByProducer(producerName: string): Movie[] {
+    return this.movieRepository.findByProducer(producerName);
+  }
+}
